8bdc8760f58d7e65b0f53de22549fdfcc61fde3d
Deepesh Jha <76602335+DeepeshKumarJha@users.noreply.github.com>
2022-05-07 13:25:11 +0530

    working on search page

--- src/App.js
+++ src/App.js
@@ -1,78 +1,15 @@
-import { lazy, Suspense, useEffect, useState } from 'react';
-import Hero from './heroSection/Hero';
-import { Box } from '@chakra-ui/react';
-import Banner from './banner/Banner';
-
-const Facts = lazy(() => {
-	return import('./facts/Facts');
-});
-const Packages = lazy(() => {
-	return import('./packages/Packages');
-});
-const SpaceBetween = lazy(() => {
-	return import('./spaceBetween/SpaceBetween');
-});
-const ImageSlider = lazy(() => {
-	return import('./imageSlider/ImageSlider');
-});
-const IconSection = lazy(() => {
-	return import('./iconSection/IconSection');
-});
-const Blog = lazy(() => {
-	return import('./blogs/Blog');
-});
-// const Partners = lazy(() => {
-// 	return import('./partners/Partners');
-// });
-const Footer = lazy(() => {
-	return import('./footer/Footer');
-});
+import { Routes, Route } from 'react-router-dom';
+import Homepage from './Homepage';
+import SearchResult from './pages/SearchResult/SearchResult';
 
 const App = () => {
-	const [load, setLoad] = useState(false);
-
-	useEffect(() => {
-		setTimeout(() => {
-			setLoad(true);
-		}, 100);
-	});
 	return (
-		<Box overflow='hidden'>
-			<Hero /> {/* Responsive Done */}
-			{load ? (
-				<>
-					<Suspense fallback={<div>Loading...</div>}>
-						<Facts /> {/* Responsive Done */}
-					</Suspense>
-					<Suspense fallback={<div>Loading...</div>}>
-						<Packages /> {/* Responsive Done*/}
-					</Suspense>
-					<Suspense fallback={<div>Loading...</div>}>
-						<SpaceBetween space='50px' />
-					</Suspense>
-					<Suspense>
-						<Banner />
-					</Suspense>
-					<Suspense fallback={<div>Loading...</div>}>
-						<ImageSlider /> {/* Responsive Done */}
-					</Suspense>
-					<Suspense fallback={<div>Loading...</div>}>
-						{/*<Partners /> {/* Responsive Done */}
-					</Suspense>
-					<Suspense fallback={<div>Loading...</div>}>
-						<IconSection />
-					</Suspense>
-					<Suspense fallback={<div>Loading...</div>}>
-						<Blog />
-					</Suspense>
-					<Suspense fallback={<div>Loading...</div>}>
-						<Footer /> {/* Responsive Done */}
-					</Suspense>
-				</>
-			) : (
-				<></>
-			)}
-		</Box>
+		<Routes>
+			<Route path='/' element={<Homepage />} />
+			<Route path='search'>
+				<Route path=':id' element={<SearchResult />} />
+			</Route>
+		</Routes>
 	);
 };
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { FromStateProvider } from './context/formStateContext';
 import './index.css';
@@ -9,9 +10,11 @@ const root_tag = document.getElementById('root');
 
 ReactDOM.render(
 	<ChakraProvider>
-		<FromStateProvider>
-			<App />
-		</FromStateProvider>
+		<BrowserRouter>
+			<FromStateProvider>
+				<App />
+			</FromStateProvider>
+		</BrowserRouter>
 	</ChakraProvider>,
 	root_tag
 );
