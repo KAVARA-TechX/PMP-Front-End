af5a8e1327326a9c00f265f9ee5eb39fcf2f1706
Deepesh Jha <76602335+DeepeshKumarJha@users.noreply.github.com>
2022-05-11 15:43:14 +0530

    blog details

--- src/pages/BlogsList/BlogDetails.jsx
+++ src/pages/BlogsList/BlogDetails.jsx
@@ -1,9 +1,18 @@
 import { Box, Text, Image } from '@chakra-ui/react';
+import parse from 'html-react-parser';
 import Footer from '../../footer/Footer';
 import Nav from '../../nav/Nav';
 import img from '../../assets/banner.png';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const BlogDetails = () => {
+	const val = useLocation();
+	const blogData = val.state;
+	console.log(blogData);
+	useEffect(() => {});
+	console.log(blogData.blogBody);
+
 	return (
 		<>
 			<Nav />
@@ -12,19 +21,10 @@ const BlogDetails = () => {
 					Heading One
 				</Text>
 				<Box h='fit-content' pb='30px'>
-					<Image src={img} mx='auto' w='90%' />
+					<Image src={blogData.imageUrl} mx='auto' w='90%' />
 				</Box>
 				<Text fontSize={20} lineHeight={1.9}>
-					Lorem Ipsum is simply dummy text of the printing and
-					typesetting industry. Lorem Ipsum has been the industry's
-					standard dummy text ever since the 1500s, when an unknown
-					printer took a galley of type and scrambled it to make a
-					type specimen book. It has survived not only five centuries,
-					but also the leap into electronic typesetting, remaining
-					essentially unchanged. It was popularised in the 1960s with
-					the release of Letraset sheets containing Lorem Ipsum
-					passages, and more recently with desktop publishing software
-					like Aldus PageMaker including versions of Lorem Ipsum.
+					{parse(blogData.blogBody)}
 				</Text>
 			</Box>
 			<Footer />
--- src/pages/BlogsList/BlogList.jsx
+++ src/pages/BlogsList/BlogList.jsx
@@ -1,22 +1,117 @@
 import { Box, Text, Image } from '@chakra-ui/react';
-
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import img from '../../assets/banner.png';
 import img2 from '../../assets/banner.jpeg';
-import Footer from '../../footer/Footer';
+// import Footer from '../../footer/Footer';
 import Nav from '../../nav/Nav';
+import parse from 'html-react-parser';
+import React, { useEffect, useState, Suspense } from 'react';
+
+const Footer = React.lazy(() => {
+	return import('../../footer/Footer');
+});
+
+const Blog = ({ data }) => {
+	const navigate = useNavigate();
+	const ca = new Date(data.createdAt);
+	const monthName = [
+		'Jan',
+		'Feb',
+		'Mar',
+		'Apr',
+		'May',
+		'Jun',
+		'Jul',
+		'Aug',
+		'Sep',
+		'Oct',
+		'Nov',
+		'Dec',
+	];
+
+	return (
+		<Box
+			display={'flex'}
+			borderTop='1px solid rgba(255,255,255,.3)'
+			borderBottom='1px solid rgba(255,255,255,.3)'
+			py={4}
+			cursor='pointer'
+		>
+			{/* this box is for data  */}
+			<Box
+				w={'60%'}
+				pr={4}
+				position='relative'
+				onClick={() => {
+					navigate(`/blogs/${data._id}`, { state: data });
+				}}
+			>
+				{/* Heading */}
+				<Text
+					display={'block'}
+					fontSize={30}
+					fontWeight={'bold'}
+					pb={5}
+					pt={4}
+				>
+					{data.blogHeading}
+				</Text>
+				{/* chunck of blog body */}
+				<Text display={'inline-block'}>
+					{/* Lorem Ipsum is simply dummy text of the printing and
+					typesetting industry. Lorem Ipsum has been the industry's
+					standard dummy text ever since the 1500s, when an unknown
+					printer took a galley of type and scrambled it to make a
+					type specimen book. */}
+					{data.blogBody
+						? data.blogBody.replace(/(<([^>]+)>)/gi, '')
+						: ''}
+				</Text>
+				<Box position={'absolute'} bottom={0}>
+					<Text display={'inline-block'} pr={5} color='gray.600'>
+						3 min Read
+					</Text>
+					<Text display={'inline-block'} color='gray.600'>
+						{monthName[ca.getMonth()]} {ca.getDate()}
+					</Text>
+				</Box>
+			</Box>
+			{/* this box is for image */}
+			<Box w='40%'>
+				<Image src={data.imageUrl} />
+			</Box>
+		</Box>
+	);
+};
 
 const BlogList = () => {
+	const [blogList, setBlogList] = useState([]);
 	const navigate = useNavigate();
 	const handleBack = () => {
 		navigate('/');
 	};
+
+	useEffect(() => {
+		const getBlogs = async () => {
+			try {
+				const response = await axios.get(
+					'https://planmyleisure.herokuapp.com/blog/get-blogs'
+				);
+				console.log(response);
+				setBlogList(response.data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		getBlogs();
+	}, []);
+
 	return (
 		<>
 			<Nav />
 			<Box
 				w='100vw'
-				h='100vh'
 				pl='5vw'
 				pr='5vw'
 				pt='7vw'
@@ -30,8 +125,11 @@ const BlogList = () => {
 					</Text>
 				</Box>
 				{/* results */}
-				<Box minH='900px' className='new-font'>
+				<Box minH='fit-content' className='new-font'>
 					{/* this blog contain a blog in a blog list */}
+					{blogList.map((blog, index) => {
+						return <Blog data={blog} key={index} />;
+					})}
 					<Box
 						display={'flex'}
 						borderTop='1px solid rgba(255,255,255,.3)'
@@ -126,7 +224,9 @@ const BlogList = () => {
 					</Box>
 				</Box>
 			</Box>
-			<Footer />
+			<Suspense fallback={'loading..'}>
+				<Footer />
+			</Suspense>
 		</>
 	);
 };
